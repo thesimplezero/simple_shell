6f169db8c049b1589e11afd4b1daeb63e37e5868
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-05-23 02:19:57 -0700

    simple_shell

---
+++ builtin_env.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <unistd.h>
+
+/* This is a pointer to an array of strings representing the environment */
+extern char **environ;
+
+/**
+ * builtin_env - Handles the env builtin
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int builtin_env(void)
+{
+	char **env = environ;
+
+/* Iterate over each environment variable and print it */
+	while (*env)
+	{
+		if (write(STDOUT_FILENO, *env, strlen(*env)) == -1)
+		{
+			perror("Error writing environment variable");
+			return (-1);
+		}
+
+	/* Print a newline after each variable */
+		if (write(STDOUT_FILENO, "\n", 1) == -1)
+		{
+			perror("Error writing newline");
+			return (-1);
+		}
+
+		env++;
+	}
+
+	return (0);
+}
+
