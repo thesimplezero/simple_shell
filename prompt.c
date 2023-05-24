94f7f7c287075601fbf11c99db45dc518010d189
thesimplezero <hi@thesimplezero>
2023-05-25 02:13:56 +0300

    edit prompt function

--- prompt.c
+++ prompt.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * print_prompt - Prints the shell prompt.
- */
+* print_prompt - Prints a shell prompt.
+*/
 void print_prompt(void)
 {
-	char cwd[1024];
+ssize_t write_res;
 
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		perror("getcwd() error");
-		return;
-	}
+write_res = write(STDOUT_FILENO, "$ ", 2);
+if (write_res == -1) {
+write(STDERR_FILENO, "Error writing prompt to stdout\n", 32);
+exit(EXIT_FAILURE);
+} else if (write_res < 2) {
+write(STDERR_FILENO, "Incomplete write of prompt to stdout\n", 37);
+exit(EXIT_FAILURE);
+}
 
-	write(STDOUT_FILENO, cwd, strlen(cwd));
-	write(STDOUT_FILENO, " $ ", 3);
-	fflush(stdout);
+if (fflush(stdout) != 0) {
+write(STDERR_FILENO, "Error flushing stdout\n", 22);
+exit(EXIT_FAILURE);
+}
 }
