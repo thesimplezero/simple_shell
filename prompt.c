ab0055519c5324f394c5ff743272e312444feac4
thesimplezero <hi@thesimplezero>
2023-05-25 02:15:32 +0300

    edit prompt function

--- prompt.c
+++ prompt.c
@@ -8,15 +8,19 @@ void print_prompt(void)
 ssize_t write_res;
 
 write_res = write(STDOUT_FILENO, "$ ", 2);
-if (write_res == -1) {
+if (write_res == -1)
+{
 write(STDERR_FILENO, "Error writing prompt to stdout\n", 32);
 exit(EXIT_FAILURE);
-} else if (write_res < 2) {
+}
+else if (write_res < 2)
+{
 write(STDERR_FILENO, "Incomplete write of prompt to stdout\n", 37);
 exit(EXIT_FAILURE);
 }
 
-if (fflush(stdout) != 0) {
+if (fflush(stdout) != 0)
+{
 write(STDERR_FILENO, "Error flushing stdout\n", 22);
 exit(EXIT_FAILURE);
 }
